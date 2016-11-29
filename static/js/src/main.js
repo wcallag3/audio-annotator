8156207375d63cc1dfc8fd090e14d8375dee238e
William Callaghan <wcallag@gmail.com>
2016-11-28 20:33:57 -0500

    Some error handling when getting annotations

--- static/js/src/main.js
+++ static/js/src/main.js
@@ -217,9 +217,9 @@ UrbanEars.prototype = {
                 // List of actions the user took to play and pause the audio
                 play_events: this.playBar.getEvents(),
                 // Global annotation data
-                global_annotations: this.globalTags.getAnnotations(),
+                global_annotations: this.globalTags ? this.globalTags.getAnnotations() : [],
                 // Global annotation events
-                global_events: this.globalTags.getEvents(),
+                global_events: this.globalTags ? this.globalTags.getEvents() : [],
                 // Boolean, if at the end, the user was shown what city the clip was recorded in
                 final_solution_shown: this.stages.aboveThreshold()
             };
